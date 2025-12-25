26011c57d1eb5295ce01ccef150dc073626d8081
Flow <16564216@qq.com>
2025-12-25 14:19:49 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: mode === 'production' ? '/pc-template-react/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
